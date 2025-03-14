94f711d3696c617f603a55cdba703ad6d301c5d3
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-03-14 18:07:59 +0530

    feat(chat): basic chat done

--- src/chat/Services/chat.service.ts
+++ src/chat/Services/chat.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
+import { Server } from 'socket.io'
+
+import configuration from 'src/config/configuration';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+    private readonly _pub: Redis;
+    private readonly _sub: Redis;
+    
+    private server: Server;
+    
+    constructor() {
+        const redisConfiguration = {
+            host: configuration().redisConfiguration.host,
+            port: configuration().redisConfiguration.port,
+          };
+      
+          this._pub = new Redis(redisConfiguration);
+          this._sub = new Redis(redisConfiguration);
+    };
+    
+
+    setServer(server: Server): void {
+        this.server = server;
+    }
+}
