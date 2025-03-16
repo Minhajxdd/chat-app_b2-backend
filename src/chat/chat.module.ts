c64103d0f9c04671912ad2ae2a244f914e40aa1a
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-16 12:32:50 +0530

    feat(chat): chat gateway registered

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './Services/chat.service';
 import { ChatController } from './Controllers/chat.controller';
+import { ChatGateway } from './Gateway/chat.gateway';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ChatGateway],
 })
 export class ChatModule {}
