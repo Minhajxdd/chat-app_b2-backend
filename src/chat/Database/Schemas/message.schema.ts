8d3922753827083faea8372f9b62b9100bf82ad7
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-16 14:12:26 +0530

    feat(chat): message schema done

---
+++ src/chat/Database/Schemas/message.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type MessageDocument = Message & Document;
+
+export enum MessageType {
+  TEXT = 'text',
+  FILE = 'file',
+}
+
+@Schema({ timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } })
+export class Message {
+  @Prop({ type: Types.ObjectId, ref: 'Conversation', required: true })
+  conversation: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  sender: Types.ObjectId;
+
+  @Prop({ type: String, enum: MessageType, default: MessageType.TEXT })
+  messageType: MessageType;
+
+  @Prop()
+  content: string;
+
+  @Prop({ type: Types.ObjectId, ref: 'File', default: null })
+  file: Types.ObjectId;
+}
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
