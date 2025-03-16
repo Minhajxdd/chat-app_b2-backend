4989e4b70bab94883d1b169098e2218f4d5f3b75
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-16 14:24:19 +0530

    feat(chat): registerd schmea

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,8 +2,39 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './Services/chat.service';
 import { ChatController } from './Controllers/chat.controller';
 import { ChatGateway } from './Gateway/chat.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Conversation,
+  ConversationSchema,
+} from './Database/Schemas/conversation.schema';
+import {
+  ConversationParticipant,
+  ConversationParticipantSchema,
+} from './Database/Schemas/conversation-participant.schema';
+import { Message, MessageSchema } from './Database/Schemas/message.schema';
+import { File, FileSchema } from './Database/Schemas/file.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Conversation.name,
+        schema: ConversationSchema,
+      },
+      {
+        name: ConversationParticipant.name,
+        schema: ConversationParticipantSchema,
+      },
+      {
+        name: Message.name,
+        schema: MessageSchema,
+      },
+      {
+        name: File.name,
+        schema: FileSchema,
+      },
+    ]),
+  ],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway],
 })
