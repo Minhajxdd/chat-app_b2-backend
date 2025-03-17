49dab59cbd9ecbd0037150de813af0682a485136
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-17 10:03:47 +0530

    feat(chat): injected userRepository in chatDetailsRepository

--- src/chat/Database/Repositories/chat-details.repository.ts
+++ src/chat/Database/Repositories/chat-details.repository.ts
@@ -1,5 +1,11 @@
-import { Injectable } from "@nestjs/common";
-import exp from "constants";
+import { Inject, Injectable } from '@nestjs/common';
+import { UserRepository } from 'src/auth/Database/Repository/user.repository';
+import { IUserRepository } from 'src/auth/Interface/Repository/user-repository.interface';
 
 @Injectable()
-export class ChatDetailsRepository {}
+export class ChatDetailsRepository {
+  constructor(
+    @Inject('UserRepository')
+    private readonly _userRepository: UserRepository,
+  ) {}
+}
