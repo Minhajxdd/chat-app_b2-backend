2fead78640875defb9badee0bcaeb79a07711e0e
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-18 15:14:18 +0530

    feat(chat): chatgroup schmea done

---
+++ src/chat/Database/Schemas/chat-group.schmea.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema()
+export class ChatGroup extends Document {
+  @Prop({ type: String, required: true })
+  title: string;
+
+  @Prop({ type: String, required: true })
+  description: string;
+
+  @Prop({ type: [Types.ObjectId], required: true })
+  admins: Types.ObjectId[];
+
+}
+
+export const ChatGroupSchema = SchemaFactory.createForClass(ChatGroup);
