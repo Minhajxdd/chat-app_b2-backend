e0190a9899ee6b750d232c467ea3f051923972bc
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-19 13:33:18 +0530

    fix(chat); removed unwanted dto from service

--- src/chat/Services/chat-http-message.service.ts
+++ src/chat/Services/chat-http-message.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { ChatGetConversationMessageDto } from '../Dto/chat-get-conversation-messages.dto';
+
 import { MessageRepository } from '../Database/Repositories/message.repository';
 
 @Injectable()
 export class ChatHttpMessageService {
   constructor(private readonly _messageRepository: MessageRepository) {}
 
-  async getConversationMessage(data: ChatGetConversationMessageDto) {
-    const { conversationId } = data;
-
+  async getConversationMessage(conversationId: string) {
     const LIMIT = 2;
 
-    const result = await this._messageRepository.getConversationsMessage(
+    const data = await this._messageRepository.getConversationsMessage(
       conversationId,
       LIMIT,
     );
 
-    console.log(result);
+    console.log(data);
   }
 }
