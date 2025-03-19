63f16f6bcb12258dcf59a42da84d95dc52819661
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-19 13:06:49 +0530

    refactor(chat): chat http service spilitted into request separate

--- src/chat/Services/chat-http.service.ts
+++ src/chat/Services/chat-http.service.ts
@@ -6,11 +6,6 @@ import {
 } from '@nestjs/common';
 import { UserRepository } from 'src/auth/Database/Repository/user.repository';
 import { ConversationParticipantsRepository } from '../Database/Repositories/conversation-participant.repository';
-import { RequestChatRepository } from '../Database/Repositories/request-chat.repository';
-import mongoose from 'mongoose';
-import { ConversationType } from '../Types/database-schmea.models';
-import { RequestActionsDto } from '../Dto/request-actions.dto';
-import { ConversationRepository } from '../Database/Repositories/conversation.repository';
 
 @Injectable()
 export class ChatHttpService {
@@ -18,8 +13,6 @@ export class ChatHttpService {
     @Inject('UserRepository')
     private readonly _userRepository: UserRepository,
     private readonly _conversationParticipantsRepository: ConversationParticipantsRepository,
-    private readonly _requestChatRepository: RequestChatRepository,
-    private readonly _conversationRepository: ConversationRepository,
   ) {}
 
   async searchByFullName(userId: string, name: string, limit?: string) {
@@ -54,95 +47,6 @@ export class ChatHttpService {
       message: 'successfully checked for conversation',
       data,
     };
-  }
-
-  async requestConversation(currentUserId: string, otherUserId: string) {
-    try {
-      if (currentUserId == otherUserId) {
-        throw new BadRequestException('Invalid Request');
-      }
-
-      await this._requestChatRepository.create({
-        requestedBy: new mongoose.Types.ObjectId(currentUserId),
-        requestedTo: new mongoose.Types.ObjectId(otherUserId),
-        type: ConversationType.SINGLE,
-      });
-
-      // Implement Notification
-
-      return {
-        status: 'success',
-        message: 'successfully sent request',
-      };
-    } catch (err) {
-      console.log(`Error while request conversation`);
-      throw new InternalServerErrorException();
-    }
-  }
-
-  async getChatRequests(userId: string) {
-    try {
-      const LIMIT = 3;
-      const requests = await this._requestChatRepository.findRequestsByUserId(
-        userId,
-        LIMIT,
-      );
-
-      return {
-        status: 'success',
-        message: 'successfully fetched data',
-        data: requests,
-      };
-    } catch (err) {
-      console.log(err);
-      throw new InternalServerErrorException();
-    }
-  }
-
-  async acceptRequests(userId: string, data: RequestActionsDto) {
-    try {
-      const { action, requestId } = data;
-
-      const request = await this._requestChatRepository.findRequestAndDelete(
-        userId,
-        requestId,
-      );
-
-      if (!request) {
-        throw new BadRequestException();
-      }
-
-      if (action === 'reject') {
-        return {
-          status: 'success',
-          message: 'Successfully rejected request',
-        };
-      }
-
-      const newConversation = await this._conversationRepository.create({
-        type: request.type,
-      });
-
-      let { _id: newConversationId } = newConversation;
-
-      await this._conversationParticipantsRepository.create({
-        conversation: new mongoose.Types.ObjectId(String(newConversationId)),
-        user: request.requestedBy,
-      });
-
-      await this._conversationParticipantsRepository.create({
-        conversation: new mongoose.Types.ObjectId(String(newConversationId)),
-        user: request.requestedTo,
-      });
-
-      return {
-        status: 'success',
-        message: 'successfull added conversation',
-      };
-    } catch (err) {
-      console.log(err);
-      throw new InternalServerErrorException();
-    }
   }
 
   async getConversations(userId: string) {
