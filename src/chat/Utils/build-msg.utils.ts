ef57ba2a915c39a394151169337493ce2f49e6d3
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-19 10:46:15 +0530

    fix(chat): formatted date converted to new date

--- src/chat/Utils/build-msg.utils.ts
+++ src/chat/Utils/build-msg.utils.ts
@@ -2,16 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BuildMessageUtils {
-  buildMsg(conversation: string, sender: string,text: string) {
+  buildMsg(conversation: string, sender: string, text: string) {
     return {
       sender,
       conversation,
       text,
-      time: new Intl.DateTimeFormat('default', {
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-      }).format(new Date()),
+      time: new Date(),
     };
   }
 }
